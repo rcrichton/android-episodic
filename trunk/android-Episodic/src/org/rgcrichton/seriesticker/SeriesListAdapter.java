29d7431c926e08f46951f04c634549808a063eec
RG.Crichton <RG.Crichton@d4e9f400-5f39-31bf-637e-4d0f0e7a2b5d>
2010-06-09 16:53:40 +0000

    Initial import

---
+++ trunk/android-Episodic/src/org/rgcrichton/seriesticker/SeriesListAdapter.java
@@ -0,0 +1,83 @@
+package org.rgcrichton.seriesticker;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.SimpleCursorAdapter;
+
+public class SeriesListAdapter extends SimpleCursorAdapter {
+	
+	private SeriesTickerDbAdapter mDbHelper;
+	private SeriesTicker seriesTicker;
+
+	public SeriesListAdapter(SeriesTicker seriesTicker, int layout, Cursor c,
+			String[] from, int[] to) {
+		super(seriesTicker, layout, c, from, to);
+		mDbHelper = new SeriesTickerDbAdapter(seriesTicker);
+		this.seriesTicker = seriesTicker;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		View view = super.getView(position, convertView, parent);
+		
+		Button seasonButton = (Button) view.findViewById(R.id.season_plus_button);
+		Button episodeButton = (Button) view.findViewById(R.id.episode_plus_button);
+		
+		seasonButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+            	Long rowId = (Long) view.getTag();
+            	mDbHelper.open();
+                mDbHelper.incrementSeason(rowId);
+                mDbHelper.close();
+                seriesTicker.refreshData();
+            }
+
+        });
+		
+		episodeButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+            	Long rowId = (Long) view.getTag();
+            	mDbHelper.open();
+            	mDbHelper.incrementEpisode(rowId);
+            	mDbHelper.close();
+            	seriesTicker.refreshData();
+            }
+
+        });
+		
+		view.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View view) {
+				View seasonButtonView = view.findViewById(R.id.season_plus_button);
+				seriesTicker.customOnListItemClick((Long)(seasonButtonView.getTag()));
+			}
+		});
+		
+		return view; 
+	}
+
+	@Override
+	public void bindView(View view, Context context, Cursor cursor) {
+		int columnIndex = cursor.getColumnIndex(SeriesTickerDbAdapter.KEY_ROWID);
+		Long rowId = cursor.getLong(columnIndex);
+		
+		View seasonButtonView = view.findViewById(R.id.season_plus_button);
+		seasonButtonView.setTag(rowId);
+		
+		View episodeButtonView = view.findViewById(R.id.episode_plus_button);
+		episodeButtonView.setTag(rowId);
+		
+		super.bindView(view, context, cursor);
+		
+	}
+	
+	
+
+}
