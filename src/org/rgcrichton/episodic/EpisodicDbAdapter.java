0ee7fe0905d01cd945e7cb686e15947e6bf59f63
supermario7@gmail.com <supermario7@gmail.com@d4e9f400-5f39-31bf-637e-4d0f0e7a2b5d>
2011-01-18 21:10:57 +0000

    Got new DB code working. No more Force Closes YAY!! :)

--- src/org/rgcrichton/episodic/EpisodicDbAdapter.java
+++ src/org/rgcrichton/episodic/EpisodicDbAdapter.java
@@ -10,6 +10,7 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
@@ -28,10 +29,10 @@ public class EpisodicDbAdapter {
 	public static final String KEY_TAG_NAME = "tag_name";
 	
 	// Series to tags table. Joins series and tags
-	private static final String SERIES_TICKER_TO_TAGS_TABLE = "series_ticker_to_tags_table";
+	private static final String SERIES_TICKER_TO_TAGS_TABLE = "series_ticker_to_tags";
 	// public static final String KEY_ROWID = "_id";
 	public static final String KEY_SERIES_TICKER_ID = "series_ticker_id";
-	public static final String KEY_TAG_ID = "tag_id";
+	public static final String KEY_TAG_ID = "tags_id";
 	
 	//The Android's default system path of your application database.
     private static final String DB_PATH = "/data/data/org.rgcrichton.episodic/databases/";
@@ -48,45 +49,91 @@ public class EpisodicDbAdapter {
         DatabaseHelper(Context context) {
             super(context, DATABASE_NAME, null, DATABASE_VERSION);
         }
-
-        @Override
-        public void onCreate(SQLiteDatabase db) {
-        	//this.getReadableDatabase();
-            
-        	try {
-	            //Open your local db as the input stream
-	        	InputStream myInput = mCtx.getAssets().open(DATABASE_NAME);
-	     
-	        	// Path to the just created empty db
-	        	String outFileName = DB_PATH + DATABASE_NAME;
-	     
-	        	//Open the empty db as the output stream
-	        	OutputStream myOutput = new FileOutputStream(outFileName);
-	     
-	        	//transfer bytes from the inputfile to the outputfile
-	        	byte[] buffer = new byte[1024];
-	        	int length;
-	        	while ((length = myInput.read(buffer)) > 0){
-	        		myOutput.write(buffer, 0, length);
-	        	}
-	     
-	        	//Close the streams
-	        	myOutput.flush();
-	        	myOutput.close();
-	        	myInput.close();
+        
+        /**
+         * Creates a empty database on the system and rewrites it with your own database.
+         * */
+        public void createDataBase() throws IOException{
+     
+        	boolean dbExist = checkDataBase();
+     
+        	if(dbExist){
+        		//do nothing - database already exist
+        	}else{
+     
+        		//By calling this method and empty database will be created into the default system path
+                   //of your application so we are gonna be able to overwrite that database with our database.
+            	this.getReadableDatabase();
+     
+            	try {
+        			copyDataBase();
+     
+        		} catch (IOException e) {
+            		throw new Error("Error copying database");
+            	}
         	}
-        	catch (IOException e) {
-        		Log.e(this.getClass().toString(), e.getMessage());
-        	}
-       }
-
-
-		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-            Log.w(this.getClass().toString(), "Upgrading database from version " + oldVersion + " to "
-                    + newVersion + ", which will destroy all old data");
-            //db.execSQL("DROP TABLE IF EXISTS " + DATABASE_TABLE);
-            //onCreate(db);
         }
+        
+        /**
+         * Copies your database from your local assets-folder to the just created empty database in the
+         * system folder, from where it can be accessed and handled.
+         * This is done by transfering bytestream.
+         * */
+        private void copyDataBase() throws IOException{
+     
+        	//Open your local db as the input stream
+        	InputStream myInput = mCtx.getAssets().open("EpisodicDBSchema.dbs");
+     
+        	// Path to the just created empty db
+        	String outFileName = DB_PATH + DATABASE_NAME;
+     
+        	//Open the empty db as the output stream
+        	OutputStream myOutput = new FileOutputStream(outFileName);
+     
+        	//transfer bytes from the inputfile to the outputfile
+        	byte[] buffer = new byte[1024];
+        	int length;
+        	while ((length = myInput.read(buffer))>0){
+        		myOutput.write(buffer, 0, length);
+        	}
+     
+        	//Close the streams
+        	myOutput.flush();
+        	myOutput.close();
+        	myInput.close();
+        }
+        
+        /**
+         * Check if the database already exist to avoid re-copying the file each time you open the application.
+         * @return true if it exists, false if it doesn't
+         */
+        private boolean checkDataBase(){
+     
+        	SQLiteDatabase checkDB = null;
+     
+        	try{
+        		String myPath = DB_PATH + DATABASE_NAME;
+        		checkDB = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READONLY);
+     
+        	}catch(SQLiteException e) {
+        		//database does't exist yet.
+        	}
+     
+        	if(checkDB != null) {
+        		checkDB.close();
+        	}
+
+        	return checkDB != null ? true : false;
+        }
+        
+        public void openDataBase() throws SQLException {
+        	//Open the database
+            String myPath = DB_PATH + DATABASE_NAME;
+        	mDb = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READWRITE);
+        }
+
+        public void onCreate(SQLiteDatabase db) {}
+		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {}
     }
     
     public EpisodicDbAdapter(Context ctx) {
@@ -95,7 +142,12 @@ public class EpisodicDbAdapter {
 
     public EpisodicDbAdapter open() throws SQLException {
         mDbHelper = new DatabaseHelper(mCtx);
-        mDb = mDbHelper.getWritableDatabase();
+        try {
+			mDbHelper.createDataBase();
+		} catch (IOException e) {
+			throw new Error("Unable to create database");
+		}
+        mDbHelper.openDataBase();
         return this;
     }
 
@@ -104,7 +156,6 @@ public class EpisodicDbAdapter {
     		mDb.close();
     	}
     }
-
 
     public long createSeries(String seriesName, Integer seasonNum, Integer episodeNum) {
         ContentValues initialValues = new ContentValues();
@@ -175,7 +226,7 @@ public class EpisodicDbAdapter {
 	}
 	
 	public Cursor fetchTags(long rowId) throws SQLException {
-		String fetchTagsQuery = "SELECT a." + KEY_ROWID + " a." + KEY_TAG_NAME + " FROM " + TAGS_TABLE + " a, " + SERIES_TICKER_TO_TAGS_TABLE + " b where a." + KEY_ROWID + 
+		String fetchTagsQuery = "SELECT a." + KEY_ROWID + ", a." + KEY_TAG_NAME + " FROM " + TAGS_TABLE + " a, " + SERIES_TICKER_TO_TAGS_TABLE + " b where a." + KEY_ROWID + 
 								" = b." + KEY_TAG_ID + " and b." + KEY_SERIES_TICKER_ID + " = " + rowId;
         Cursor mCursor = mDb.rawQuery(fetchTagsQuery, null);
         if (mCursor != null) {
