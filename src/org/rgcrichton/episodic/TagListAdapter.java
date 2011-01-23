172ff58cc33d56e49c818be4e02822df20180bd6
rg.crichton@gmail.com <rg.crichton@gmail.com@d4e9f400-5f39-31bf-637e-4d0f0e7a2b5d>
2011-01-23 19:53:32 +0000

    Added support for adding and removing tags to series. The tags are displayed when editing the series and an activity to add tags and edit the tags associated with a series have been developed. Booya :P

--- src/org/rgcrichton/episodic/TagListAdapter.java
+++ src/org/rgcrichton/episodic/TagListAdapter.java
@@ -1,29 +1,70 @@
 package org.rgcrichton.episodic;
 
+import android.content.Context;
 import android.database.Cursor;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.SimpleCursorAdapter;
 
 public class TagListAdapter extends SimpleCursorAdapter {
 	
 	private EpisodicDbAdapter mDbHelper;
 	private TagList tagList;
-
+	private long seriesRowId;
+	
 	public TagListAdapter(TagList tagList, int layout, Cursor c,
-			String[] from, int[] to) {
+			String[] from, int[] to, long seriesRowId) {
 		super(tagList, layout, c, from, to);
-		mDbHelper = new EpisodicDbAdapter(tagList);
+		
+		this.mDbHelper = new EpisodicDbAdapter(tagList);
 		this.tagList = tagList;
+		this.seriesRowId = seriesRowId;
+	}
+
+	@Override
+	public void bindView(View view, Context context, Cursor cursor) {
+		int columnIndex = cursor.getColumnIndex(EpisodicDbAdapter.KEY_ROWID);
+		Long tagRowId = cursor.getLong(columnIndex);
+		
+		View tagCheckbox = view.findViewById(R.id.tag_checkbox);
+		tagCheckbox.setTag(tagRowId);
+		
+		super.bindView(view, context, cursor);
 	}
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
+		
 		View view = super.getView(position, convertView, parent);
 		
+		CheckBox checkbox = (CheckBox) view.findViewById(R.id.tag_checkbox);
 		
+		mDbHelper.open();
+		checkbox.setChecked(mDbHelper.hasTag(seriesRowId, (Long) checkbox.getTag()));
+		mDbHelper.close();
+		
+		checkbox.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				long tagRowId = (Long) buttonView.getTag();
+				if (isChecked) {
+					mDbHelper.open();
+					mDbHelper.addTagToSeries(seriesRowId, tagRowId);
+					mDbHelper.close();
+				} else {
+					mDbHelper.open();
+					mDbHelper.removeTagFromSeries(seriesRowId, tagRowId);
+					mDbHelper.close();
+				}
+			}
+		});
 		
 		return view;
 	}
+	
+	
 
 }
